84144c827cfe0752e20afa2c58ddb4d056bc96ea
greg-olivier <greg.olivier@hotmail.fr>
2025-11-02 20:26:13 +0100

    feat(fizzbuzz): implement algo

---
+++ fizzbuzz/FizzBuzz.js
@@ -0,0 +1,21 @@
+function isDivisibleBy(number, divisor) {
+    return number % divisor === 0;
+}   
+
+function evaluateNumber(n) {
+    let result = '';
+    if (isDivisibleBy(n, 3)) result += 'Fizz';
+    if (isDivisibleBy(n, 5)) result += 'Buzz';
+    if (isDivisibleBy(n, 7)) result += 'Bazz';
+    return result || n;
+}
+
+function fizzBuzz(n) {
+    let result = '';
+    for (let i = 1; i <= n; i++) {
+        result += evaluateNumber(i);
+    }
+    return result;
+}
+
+console.log(fizzBuzz(20));
